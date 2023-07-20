5dba1d6821814046b7f895c694f367ad1af5b10d
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-07-20 12:22:19 +0530

    Update config.py

--- SankiMusic/utilities/config/config.py
+++ SankiMusic/utilities/config/config.py
@@ -64,20 +64,20 @@ clean = {}
 autoclean = []
 ############################
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/83cfcc36ef441e516d28f.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/6abc32c3858f205434cd5.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/88f1874955694bace2760.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/88f1874955694bace2760.jpg")
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-GLOBAL_IMG_URL = "https://te.legra.ph/file/6abc32c3858f205434cd5.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/6abc32c3858f205434cd5.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+GLOBAL_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
 
 
 def time_to_seconds(time):
@@ -103,9 +103,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "SankiMusic/resource/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/6abc32c3858f205434cd5.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "SankiMusic/resource/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/83cfcc36ef441e516d28f.jpg"
+            START_IMG_URL = "https://telegra.ph/file/88f1874955694bace2760.jpg"
